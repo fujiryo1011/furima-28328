767d52780d75081fde9b1597b8d0ae31c5f7d821
fujiryo1011 <sphere3751011@gmail.com>
2020-08-23 12:48:25 +0900

    レビュー依頼修正1回目（ユーザー管理機能）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  zenkaku = /\A[ぁ-んァ-ヶー一-龠]+\z/
+  zenkaku_kana = /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/
 
-  validates :nickname, presence: true
+  with_options presence:true do
+    validates :nickname
+    validates :firstname, format: { with: zenkaku }
+    validates :lastname, format: { with: zenkaku }
+    validates :first_name_kana, format: { with: zenkaku_kana }
+    validates :last_name_kana, format: { with: zenkaku_kana }
+    validates :birthday
+  end
+  
   # validates :email, presence: true, uniqueness: true, inclusion: { in: "@" }
   validates :password, length: { minimum: 6}, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/ }
-  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/ }
-  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/ }
-  validates :birthday, presence: true
-
 end
