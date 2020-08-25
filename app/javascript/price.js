adc2ecd88486b0a19c122d2b86af8f5c2101b46a
fujiryo1011 <sphere3751011@gmail.com>
2020-08-25 18:21:31 +0900

    商品出品機能の修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,6 +1,5 @@
 window.addEventListener("load", function() {
   const item_price = document.getElementById("item-price")
-  console.log(item_price)
   const add_tax_price = document.getElementById("add-tax-price")
   const profit = document.getElementById("profit")
   item_price.addEventListener("keyup", function(){
