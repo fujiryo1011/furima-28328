1e4146aa8b4d0cfa7a88ac045fdd401d6289e716
fujiryo1011 <sphere3751011@gmail.com>
2020-08-26 12:28:12 +0900

    商品一覧表示まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,13 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def create
     item = Item.new(item_params)
-    if item.save?
+    if item.save
       redirect_to root_path
     else
       render :new
